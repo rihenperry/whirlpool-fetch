0ea82adbe0ba4bd190b8e0919e5ba8999536b70b
Rihan <rihen234@gmail.com>
2019-07-12 16:18:55 -0700

    Revert "changed hostanme to solve rabbitmq connection obstruction form clients"
    This reverts commit fedea44cfbe1f2a309814238c442f0d9b1817b14.

--- src/consumer.js
+++ src/consumer.js
@@ -1,39 +1,73 @@
+/*
+ * Copyright (C) Rihan Pereira <rihen234@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/* eslint global-require: 'warn' */
+
+import config from './helpers/config';
 import logger from './helpers/applogging';
-import {fetcherPublish as publish} from './publish';
 
-export let fetcherConsume = async function({connection, consumeChannel, publishChannel}) {
-  return new Promise((resolve, reject) => {
-    consumeChannel.consume("urlfrontier_p.to.fetcher_c", async function(msg) {
-      let msgBody = msg.content.toString();
-      let data = JSON.parse(msgBody);
-      let ans;
+const amqp = require('amqplib');
 
-      logger.log('info', 'fetch consumer received request to process ', JSON.stringify(data));
-
-      // process the request
-
-      // publish to next exchange in the chain for further processing
-      try {
-        ans = await publish(publishChannel);
-        logger.log('info', 'published result %s', ans);
-
-        await consumeChannel.ack(msg);
-        resolve('consumer msg acknowledged');
-
-      } catch(e) {
-        logger.log('error', 'occured while publishing %s', e);
-      }
-
-    });
-
-    // handle connection closed
-    connection.on("close", (err) => {
-      return reject(err);
-    });
-
-    // handle errors
-    connection.on("error", (err) => {
-      return reject(err);
-    });
-  });
-}
+logger.log('info', 'logging inside consumer script');
+//// RabbitMQ connection string
+//const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
+//
+//async function setup() {
+//  console.log("Setting up RabbitMQ Exchanges/Queues");
+//
+//  // connect to RabbitMQ Instance
+//  let connection = await amqp.connect(messageQueueConnectionString);
+//
+//  // create a channel
+//  let channel = await connection.createChannel();
+//
+//   // create 3 direct exchanges
+//  await channel.assertExchange("due.ex.frontier", "direct", { durable: true });
+//
+//  await channel.assertExchange("fetcher.ex.parser", "direct", { durable: true });
+//
+//  await channel.assertExchange("urlfilter.ex.due", "direct", { durable: true });
+//
+//
+//   // create queues
+//  await channel.assertQueue("frontier.q", { durable: true });
+//  await channel.assertQueue("fetcher.q", { durable: true });
+//
+//  await channel.assertQueue("parser.q", { durable: true });
+//  await channel.assertQueue("contentseen.q", { durable: true });
+//  await channel.assertQueue("urlfilter.q", { durable: true });
+//
+//  await channel.assertQueue("due.p", { durable: true });
+//
+//  // bind queues to due.ex.frontier exchange with a given key
+//  await channel.bindQueue("frontier.q","due.ex.frontier", "due_p.to.frontier_c");
+//  await channel.bindQueue("fetcher.q","due.ex.frontier", "frontier_p.to.fetcher_c");
+//
+//   // bind queues to fetcher.ex.parser exchange with a given key
+//  await channel.bindQueue("parser.q","fetcher.ex.parser", "fetcher_p.to.parser_c");
+//  await channel.bindQueue("contentseen.q","fetcher.ex.parser", "parser_p.to.contentseen_c");
+//  await channel.bindQueue("urlfilter.q","fetcher.ex.parser", "parser_p.to.urlfilter_c");
+//
+//  // bind queues to urlfilter.ex.due exchange with a given key
+//  await channel.bindQueue("due.q","urlfilter.ex.due", "urlfilter_p.to.due_c");
+//
+//  console.log("finished creating required exchanges, bindings, queus");
+//  process.exit();
+//}
+//
+//setup();
+//
